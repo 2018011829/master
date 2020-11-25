fc4e16fdc396e26bbdd25b54f8b2662d1e298bab
liurufeng666 <2221336136@qq.com>
2020-11-25 16:28:01 +0800

    lrf_update_android

--- project/android/ProjectTraining/app/src/main/java/com/example/projecttraining/idiom/activitys/SearchIdiomActivity.java
+++ project/android/ProjectTraining/app/src/main/java/com/example/projecttraining/idiom/activitys/SearchIdiomActivity.java
@@ -2,12 +2,18 @@ package com.example.projecttraining.idiom.activitys;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.projecttraining.R;
+import com.example.projecttraining.idiom.adapter.IdiomResultAdapter;
+import com.example.projecttraining.idiom.api.SearchIdiom;
+import com.example.projecttraining.idiom.entity.Result;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -19,8 +25,13 @@ import butterknife.OnClick;
  */
 public class SearchIdiomActivity extends AppCompatActivity {
 
+    private List<Result> resultList;
+    private IdiomResultAdapter resultAdapter;
+    private String keyword;
+
+    @BindView(R.id.tv_search_idiom) EditText etKeyword;
     @BindView(R.id.cancel_searchIdiom) Button btnCancelSearchIdiom;
-    @BindView(R.id.lv_idiom_search_result) ListView searchResultList;
+    @BindView(R.id.lv_idiom_search_result) ListView lvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +40,26 @@ public class SearchIdiomActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
+        initView();
+
     }
 
+    //用户取消搜索
     @OnClick(R.id.cancel_searchIdiom)
     public void cancelSearch(){
         finish();
         Toast.makeText(getBaseContext(),"您取消了搜索",Toast.LENGTH_SHORT).show();
+    }
+
+
+    private void initView(){
+        keyword = etKeyword.getText().toString().trim();
+        try {
+            resultList = SearchIdiom.GetIdiom(keyword);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        resultAdapter = new IdiomResultAdapter(this,resultList,R.layout.listview_idiom_search_result);
+        lvResult.setAdapter(resultAdapter);
     }
 }
--- project/android/ProjectTraining/app/src/main/java/com/example/projecttraining/idiom/entity/IdiomResult.java
+++ project/android/ProjectTraining/app/src/main/java/com/example/projecttraining/idiom/entity/IdiomResult.java
@@ -1,9 +1,46 @@
 package com.example.projecttraining.idiom.entity;
 
+import java.util.List;
 
 /**
  * 2020-11-25
  * @author lrf
  */
 public class IdiomResult {
+    private int status;
+    private String msg;
+    private List<Result> result;
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public List<Result> getResult() {
+        return result;
+    }
+
+    public void setResult(List<Result> result) {
+        this.result = result;
+    }
+
+    @Override
+    public String toString() {
+        return "IdiomResult{" +
+                "status=" + status +
+                ", msg='" + msg + '\'' +
+                ", result=" + result +
+                '}';
+    }
 }
