e4d491af30fce85125796b172255220ecbe73888
liurufeng666 <2221336136@qq.com>
2020-11-25 10:09:34 +0800

    lrf

--- project/javaEE/ProjectTraining/src/com/group/tiantian/classifyidiom/dao/ClassifyIdiomDaoImpl.java
+++ project/javaEE/ProjectTraining/src/com/group/tiantian/classifyidiom/dao/ClassifyIdiomDaoImpl.java
@@ -13,34 +13,36 @@ import com.group.tiantian.util.DBUtil;
 
 /**
  * 2020-11-25
+ * 
  * @author lrf
  */
 public class ClassifyIdiomDaoImpl {
 
 	/**
 	 * 获取所有分类父菜单和其对应的子菜单
+	 * 
 	 * @return
 	 */
-	public LinkedHashMap<ClassifyIdiom,List<ClassifyIdiom>> findAllClassifyIdiom(){
+	public LinkedHashMap<ClassifyIdiom, List<ClassifyIdiom>> findAllClassifyIdiom() {
 		Connection conn = null;
 		PreparedStatement pstm = null;
 		ResultSet rs = null;
-		LinkedHashMap<ClassifyIdiom,List<ClassifyIdiom>> map = new LinkedHashMap<>();
+		LinkedHashMap<ClassifyIdiom, List<ClassifyIdiom>> map = new LinkedHashMap<>();
 		conn = DBUtil.getConnection();
-		//查询父菜单
+		// 查询父菜单
 		try {
 			pstm = conn.prepareStatement("select * from classifyidiom where parentId = 0");
 			rs = pstm.executeQuery();
-			while(rs.next()) {
+			while (rs.next()) {
 				ClassifyIdiom classifyIdiom = new ClassifyIdiom();
 				classifyIdiom.setId(rs.getInt(1));
 				classifyIdiom.setClassifyName(rs.getString(2));
-				//查询子菜单
+				// 查询子菜单
 				pstm = conn.prepareStatement("select * from classifyidiom where parentId = ?");
 				pstm.setInt(1, classifyIdiom.getId());
 				ResultSet rsChild = pstm.executeQuery();
 				List<ClassifyIdiom> list = new ArrayList<>();
-				while(rsChild.next()) {
+				while (rsChild.next()) {
 					ClassifyIdiom cIdiom = new ClassifyIdiom();
 					cIdiom.setId(rsChild.getInt(1));
 					cIdiom.setClassifyName(rsChild.getString(2));
@@ -52,10 +54,29 @@ public class ClassifyIdiomDaoImpl {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
-			DBUtil.close(rs,pstm,conn);
+			DBUtil.close(rs, pstm, conn);
 		}
 		return map;
 	}
-	
-	
+
+	public int findClassificationByClassifyName(String classifyName) {
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		ResultSet rs = null;
+		int classifyId = 0;
+		try {
+			conn = DBUtil.getConnection();
+			pstm = conn.prepareStatement("select * from classifyidiom where classifyName='" + classifyName + "'");
+			rs = pstm.executeQuery();
+			while (rs.next()) {
+				classifyId = rs.getInt(1);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DBUtil.close(rs, pstm, conn);
+		}
+		return classifyId;
+	}
+
 }
