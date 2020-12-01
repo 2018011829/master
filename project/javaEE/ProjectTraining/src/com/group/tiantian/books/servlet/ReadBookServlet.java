a5fb2539083c8a79e0ae163a34423eb9d5acc999
lige9 <2396449772@qq.com>
2020-12-01 10:43:59 +0800

    lg

--- project/javaEE/ProjectTraining/src/com/group/tiantian/books/servlet/ReadBookServlet.java
+++ project/javaEE/ProjectTraining/src/com/group/tiantian/books/servlet/ReadBookServlet.java
@@ -20,78 +20,127 @@ import com.group.tiantian.entity.Content;
 @WebServlet("/ReadBookServlet")
 public class ReadBookServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ReadBookServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public ReadBookServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
 		response.setContentType("text/html;charset=utf-8");
-		//获取章节信息
-		String currentInfo=request.getParameter("currentContent");
-		String nextInfo=request.getParameter("nextContent");
-		if(currentInfo!=null && !currentInfo.equals("") && nextInfo!=null && !nextInfo.equals("")) {
-			Content currentContent=new Gson().fromJson(currentInfo, Content.class);
-			Content nextContent=new Gson().fromJson(nextInfo, Content.class);
-			//根据章节信息 获取书的章节内容
-			String text=getBookTextByContent(currentContent, nextContent);
+		// 获取章节信息
+		String currentInfo = request.getParameter("currentContent");
+		String nextInfo = request.getParameter("nextContent");
+		if (currentInfo != null && !currentInfo.equals("") && nextInfo != null && !nextInfo.equals("")) {
+			String text = "";
+			// 判断是否为最后一页
+			if (nextInfo.equals(currentInfo)) {
+				Content currentContent = new Gson().fromJson(currentInfo, Content.class);
+				// 根据章节信息 获取书的章节内容
+				text = getBookTextByContent(currentContent);
+			} else {
+				Content currentContent = new Gson().fromJson(currentInfo, Content.class);
+				Content nextContent = new Gson().fromJson(nextInfo, Content.class);
+				// 根据章节信息 获取书的章节内容
+				text = getBookTextByContent(currentContent, nextContent);
+			}
 			System.out.println(text);
 			response.getWriter().write(text);
-		}else {
+		} else {
 			System.out.println("未获取到章节信息！");
 		}
-		
+
 	}
-	
+
 	/**
 	 * 根据章节信息获取书的章节文字内容
+	 * 
 	 * @param content
 	 * @return
 	 */
-	public String getBookTextByContent(Content currentContent,Content nextContent) {
-		String text="";
-		//获取书的文件名
-		String fileName=currentContent.getArticleName();
-		//获取文件的存储路径
-		String filePath=getServletContext().getRealPath("/books/")+fileName;
+	public String getBookTextByContent(Content currentContent, Content nextContent) {
+		String text = "";
+		// 获取书的文件名
+		String fileName = currentContent.getArticleName();
+		// 获取文件的存储路径
+		String filePath = getServletContext().getRealPath("/books/") + fileName;
 		System.out.println(filePath);
-		//获取开始的行数
-		int start=currentContent.getStart();
-		//获取结束的行数
-		int end=nextContent.getStart();
-		//按行读取文件
-		StringBuffer buffer=new StringBuffer();
-		String temp="";
+		// 获取开始的行数
+		int start = currentContent.getStart();
+		// 获取结束的行数
+		int end = nextContent.getStart();
+		// 按行读取文件
+		StringBuffer buffer = new StringBuffer();
+		String temp = "";
 		try {
-			BufferedReader br=new BufferedReader(new InputStreamReader(new FileInputStream(filePath),"utf-8"));
-			int row=1;
-			while((temp=br.readLine())!=null) {
-				if(start<=row && row<end) {
+			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(filePath), "utf-8"));
+			int row = 1;
+			while ((temp = br.readLine()) != null) {
+				if (start <= row && row < end) {
 					buffer.append(temp);
 				}
 				row++;
 			}
-			text=buffer.toString();
+			text = buffer.toString();
 			br.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return text;
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * 根据章节信息获取书的章节文字内容
+	 * 
+	 * @param content
+	 * @return
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public String getBookTextByContent(Content currentContent) {
+		String text = "";
+		// 获取书的文件名
+		String fileName = currentContent.getArticleName();
+		// 获取文件的存储路径
+		String filePath = getServletContext().getRealPath("/books/") + fileName;
+		System.out.println(filePath);
+		// 获取开始的行数
+		int start = currentContent.getStart();
+		// 按行读取文件
+		StringBuffer buffer = new StringBuffer();
+		String temp = "";
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(filePath), "utf-8"));
+			int row = 1;
+			while ((temp = br.readLine()) != null) {
+				if (start <= row) {
+					buffer.append(temp);
+				}
+				row++;
+			}
+			text = buffer.toString();
+			br.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return text;
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
