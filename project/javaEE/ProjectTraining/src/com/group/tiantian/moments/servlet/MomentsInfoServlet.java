692b13050cf26b5ddce23f97aff45505108bae0c
2018011829 <370667742@qq.com>
2020-12-01 18:41:52 +0800

    lizhe

---
+++ project/javaEE/ProjectTraining/src/com/group/tiantian/moments/servlet/MomentsInfoServlet.java
@@ -0,0 +1,58 @@
+package com.group.tiantian.moments.servlet;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+
+/**
+ * Servlet implementation class MomentsInfoServlet
+ */
+@WebServlet("/MomentsInfoServlet")
+public class MomentsInfoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public MomentsInfoServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// 设置编码方式
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		// 获取请求数据
+		String content = request.getParameter("content");
+		String pictueUrl = request.getParameter("pictueUrl");
+		System.out.println("收到数据:" + content + ":" + pictueUrl);
+		// 返回响应
+		response.getWriter().write("收到数据：" + content + "--" + pictueUrl);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
