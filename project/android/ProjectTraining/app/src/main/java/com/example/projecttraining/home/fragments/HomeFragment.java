bea9ab32f106dc8df2c8ba3ae4d628ede82fb3d3
软件六班申亚雨 <44659564+xiaoyaojianjian@users.noreply.github.com>
2020-12-16 15:06:58 +0800

    shenyayu

--- project/android/ProjectTraining/app/src/main/java/com/example/projecttraining/home/fragments/HomeFragment.java
+++ project/android/ProjectTraining/app/src/main/java/com/example/projecttraining/home/fragments/HomeFragment.java
@@ -122,7 +122,7 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
                         }
                     }
                 };
-            }
+        }
         });
 
         //阅读
